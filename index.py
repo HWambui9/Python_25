5b05816d56460d36ba4edc8ad7b92e1c856ec1b6
Stacy <wambui.mungaiswm@gmail.com>
2025-06-11 02:19:13 +0300

    loops

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
-def addNumbers(number):
-     return(number +2)
+# def addNumbers(number):
+#      return(number +2)
 
-result = addNumbers(13)
+# result = addNumbers(13)
 
-print ("The New Number Is Equal to:",result)
+# print ("The New Number Is Equal to:",result)
+
+def areaCircle(radius) :
+     return 3.142 *radius*radius
+
+area = areaCircle(17)
+print (f"The area of the circle: {area}")
